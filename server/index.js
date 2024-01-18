d5c150731bd0dc5b4b32331bb1f5a5a452cba074
Bhut Vasu <87259010+bhut-vasu@users.noreply.github.com>
2024-01-18 10:44:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
-const nodemailer = require("nodemailer");
 const express = require("express");
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-const cors = require("cors");
-const http = require("http");
 
 const app = express();
-
-const server = http.createServer(app);
-app.use(cors({ origin: true }));
 app.use(express.json());
-app.use(cors());
+
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
+});
 
 // connecting database
 mongoose
-  .connect("mongodb+srv://admin:admin@cluster0.ge4y8ie.mongodb.net/?retryWrites=true&w=majority")
+  .connect(
+    "mongodb+srv://database:database@portfoliodatabase.qog6w.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("Database Connected Successfully");
   })
@@ -22,11 +25,11 @@ mongoose
     console.log("Error in Database Connectivity");
   });
 
-app.get("/", async (req, res, next) => {
-  res.json("Hey There, You are on a e-commerce server")
-});
-
 var user = require("./Schema.js");
+
+app.get("/", async (req, res, next) => {
+  res.json("Hey There, You are on a Ecommerce Server !!!");
+});
 
 app.post("/", async (req, res, next) => {
   const data = new user({
@@ -43,18 +46,18 @@ app.post("/", async (req, res, next) => {
       secure: true,
       auth: {
         user: "rajadipatidar227@gmail.com",
-        pass: "ilre donl ziku uhih",
+        pass: "vnpn jgjl ynpe qyaa",
       },
     });
 
     const mailOptions = {
       from: "rajadipatidar227@gmail.com",
-      to: "vasubhut157@gmail.com", 
+      to: "vasubhut157@gmail.com",
       subject: "Just Yours", // Subject line
-      text: `Email From ${req.body.email} for Newsletter`, 
+      text: `Email From ${req.body.email} for Newsletter`,
     };
 
-    transporter.sendMail(mailOptions, (eri) => {
+    transporter.sendMail(mailOptions, (err) => {
       if (err) {
         console.log(err);
       } else {
@@ -64,6 +67,7 @@ app.post("/", async (req, res, next) => {
 
     res.send({ user: true, data: result });
   } catch (e) {
+    console.log(e);
     res.send({ user: false });
   }
 });
